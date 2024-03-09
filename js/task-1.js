3bb022873ec2e567bbf8335c6600584bb6cc2ef0
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-03-09 22:53:44 +0100

    sss

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,12 @@
-const =
+const categories = document.getElementById('categories');
+const items = categories.querySelectorAll('.item');
+
+
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach(item => {
+    const list = item.querySelector(`h2`).textContent;
+    const itemCount = item.querySelectorAll(`li`).length;
+    console.log(`Category: ${list}`);
+    console.log(`Elements: ${itemCount}`);
+})
